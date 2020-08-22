4fc001d70260512954822916800885a5779080e3
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-08-22 15:54:55 +0200

    test loop 5 num

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
     /////  HW#11 Asynchrony, Promises  /////
+
+
+let i = 0;
+let time = 1000;
+for (;i<4; i++, time = time+2000) {
+  setTimeout (function() {
+    let num = Math.random().toString();
+    num = (num.substr(num.length - 5));
+    console.log(num);
+  }, time)
+}
